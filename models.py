8a80d34a99e883582266f7db9f2f024ec0709a17
Ebtihal-Saleh <gifted.e.111@gmail.com>
2019-04-05 07:49:57 +0300

    Create Items table with a serialize function

--- models.py
+++ models.py
@@ -25,3 +25,31 @@ class Categories(Base):
              'id': self.id,
              'name': self.name,
         }
+
+
+class Items(Base):
+    __tablename__ = 'items'
+    id = Column(Integer, primary_key=True)
+    cat_id = Column(Integer, ForeignKey('categories.id'))
+    categories = relationship(Categories)
+    title = Column(String, nullable=False)
+    description = Column(String)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    category = relationship(Categories, backref='items')
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'cat_id': self.cat_id,
+            'description': self.description,
+            'title': self.title,
+            'user_id': self.user_id
+        }
+
+engine = create_engine('sqlite:///ItemCatalog.db')
+
+
+Base.metadata.create_all(engine)
