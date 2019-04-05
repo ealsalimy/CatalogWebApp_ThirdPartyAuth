ed17917b832e10a430e3195f85fa248a77f4f63c
Ebtihal-Saleh <gifted.e.111@gmail.com>
2019-04-05 07:45:19 +0300

    Create User table

--- models.py
+++ models.py
@@ -4,3 +4,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 import random
 import string
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(32), index=True)
+    email = Column(String(32), index=True)
