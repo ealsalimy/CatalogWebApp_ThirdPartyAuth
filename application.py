8b708f8f48eb11ae82f9dca706fe70ea09ddeb4c
Ebtihal-Saleh <gifted.e.111@gmail.com>
2019-04-05 08:18:46 +0300

    Build auth link to Google account

--- application.py
+++ application.py
@@ -25,10 +25,31 @@ app.secret_key = os.urandom(32)
 
 @app.route('/catalog', methods=['GET', 'POST'])
 def catalog():
+    categories = session.query(Categories).all()
+    items = session.query(Items).order_by(desc(Items.id)).limit(10).all()
+    category_list = []
+    '''
+    make a list of dictionary to store the categories
+    names and to be able to display the category
+    name with each item
+    '''
+    for c in categories:
+        category_list.append(dict(id=c.id, name=c.name))
     # Create anti-forgery state token
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
                     for x in xrange(32))
     login_session['state'] = state
+    # Build authentication link to google account
+    url = 'https://accounts.google.com/o/oauth2/v2/auth?'
+    url += 'client_id=354270557828-caritkd8lfe68d1bsdevphcs6i1i30qd.'
+    url += 'apps.googleusercontent.com&'
+    url += 'response_type=code&'
+    url += 'scope=openid%20email&'
+    url += 'redirect_uri=http://localhost:5000/google_connect&'
+    url += 'state=%s&' % state
+    return render_template('public_catalog.html', categories=categories,
+                           items=items, category_list=category_list,
+                           STATE=state, link=url)
 
 
 
